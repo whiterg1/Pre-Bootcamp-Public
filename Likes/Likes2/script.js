368dab35612eab001dbe26ba8a827f12806ebccf
Roy White <roy.g.white8@icloud.com>
2021-04-27 20:19:54 -0500

    Likes

---
+++ Likes/Likes2/script.js
@@ -0,0 +1,27 @@
+var likes1 = 0;
+var likeButton1 = document.querySelector("#likeButton1");
+var likeCounter1 = document.querySelector("#likeCounter1");
+function add1(){
+    likes1++;
+    likeCounter1.innerHTML= likes1 + "like(s)"
+    console.log(likes1);
+}
+
+var likes2 = 0;
+var likeButton2 = document.querySelector("#likeButton2");
+var likeCounter2 = document.querySelector("#likeCounter2");
+function add2(){
+    likes2++;
+    likeCounter2.innerHTML= likes2 + "like(s)"
+    console.log(likes2);
+}
+
+var likes3 = 0;
+var likeButton3 = document.querySelector("#likeButton3");
+var likeCounter3 = document.querySelector("#likeCounter3");
+function add3(){
+    likes3++;
+    likeCounter3.innerHTML= likes3 + "like(s)"
+    console.log(likes3);
+}
+
